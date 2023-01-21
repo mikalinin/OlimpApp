6af5438887031387823ad35e2f89260268249278
User <artem.udovenko@yandex.ru>
2023-01-21 23:44:18 +0300

    P

--- app/src/main/java/com/example/olimpv2/MainActivity.kt
+++ app/src/main/java/com/example/olimpv2/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.olimpv2
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.L
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.res.TypedArrayUtils.getText
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -25,6 +28,26 @@ class MainActivity : AppCompatActivity() {
             if (edit_login.text.toString()=="admin" && edit_password.text.toString()=="admin"){
                 startActivity(IntentHome)
             }
+        }
+    }
+
+    //Мое
+    fun loginQuery():Boolean{
+        val edit_login: EditText= findViewById(R.id.edit_login)
+        val edit_password: EditText=findViewById(R.id.edit_password1)
+        if (edit_login.getText().toString()=="" || edit_password.getText().toString()==""){
+            return false
+        }
+        return true
+    }
+    fun onLogin(){
+        val toast = Toast.makeText(this,"Некоторые данные заполнены некорректно!",Toast.LENGTH_SHORT)
+        if (loginQuery()) {
+            val home_intent = Intent(this, home::class.java)
+            startActivity(home_intent)
+        }
+        else {
+            toast.show()
         }
     }
 }
