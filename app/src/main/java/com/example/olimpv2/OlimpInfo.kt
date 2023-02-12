2c5991206aab269366f6bcbb8632560ea9a4c934
User <artem.udovenko@yandex.ru>
2023-02-12 23:34:56 +0300

    Дизаен

--- app/src/main/java/com/example/olimpv2/OlimpInfo.kt
+++ app/src/main/java/com/example/olimpv2/OlimpInfo.kt
@@ -13,11 +13,11 @@ class OlimpInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_olimp_info)
-        val text: TextView =findViewById(R.id.text1)
-        val mapper = jacksonObjectMapper()
-        val jsonString = """{"title": "\u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0430\u044f \u043c\u0430\u0442\u0435\u043c\u0430\u0442\u0438\u0447\u0435\u0441\u043a\u0430\u044f \u043e\u043b\u0438\u043c\u043f\u0438\u0430\u0434\u0430", "subjects": ["\u041c\u0430\u0442\u0435\u043c\u0430\u0442\u0438\u043a\u0430"], "classes": [8, 9, 10, 11], "rating": 9.0, "link": "https://olimpiada.ru/activity/1", "time_table": {}}"""
-        val Olimp1: OlimpClass = mapper.readValue(jsonString)
-        text.text=Olimp1.title
+//        val text: TextView = findViewById(R.id.text1)
+//        val mapper = jacksonObjectMapper()
+//        val jsonString ="""{"title": "\u041c\u043e\u0441\u043a\u043e\u0432\u0441\u043a\u0430\u044f \u043c\u0430\u0442\u0435\u043c\u0430\u0442\u0438\u0447\u0435\u0441\u043a\u0430\u044f \u043e\u043b\u0438\u043c\u043f\u0438\u0430\u0434\u0430", "subjects": ["\u041c\u0430\u0442\u0435\u043c\u0430\u0442\u0438\u043a\u0430"], "classes": [8, 9, 10, 11], "rating": 9.0, "link": "https://olimpiada.ru/activity/1", "time_table": {}}"""
+//        val Olimp1: OlimpClass = mapper.readValue(jsonString)
+//        text.text = Olimp1.title
 
 
     }
--- app/src/main/java/com/example/olimpv2/register.kt
+++ app/src/main/java/com/example/olimpv2/register.kt
@@ -1,8 +1,10 @@
  package com.example.olimpv2
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -14,10 +16,14 @@ import com.fasterxml.jackson.module.kotlin.jsonMapper
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         val edit_name: EditText = findViewById(R.id.edit_name)
-        val edit_class: EditText = findViewById(R.id.edit_class)
-        val edit_login: EditText = findViewById(R.id.edit_login_reg)
+        val edit_class: EditText = findViewById(R.id.edit_password)
         //val User = UserClass(edit_name.text.toString(), edit_class.text.toString().toInt(), edit_login.text.toString())
         //val userJson = jsonMapper().writeValueAsString(User)
+        val reg_button: Button = findViewById(R.id.button2)
+        reg_button.setOnClickListener{
+            var Intent_Home = Intent(this, home::class.java)
+            startActivity(Intent_Home)
+        }
 
     }
 }
